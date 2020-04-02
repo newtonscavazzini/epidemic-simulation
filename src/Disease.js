778764f4499e9f504d6a03269803c630fd7b77a5
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-04-02 00:24:29 -0300

    Creating Disease class

---
+++ src/Disease.js
@@ -0,0 +1,11 @@
+class Disease {
+
+    constructor() {
+        this.fatalityRate = 3;
+        this.incubationPeriod = int(random(2, 14));
+        this.daysToRecover = incubationPeriod + int(random(7, 30));
+
+        this.day = 0;
+    }
+
+}
